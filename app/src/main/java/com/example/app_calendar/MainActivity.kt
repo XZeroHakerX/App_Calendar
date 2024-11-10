2ad337cb2ace925da190347cd5bd181c7ce0d2bf
XZeroHakerX <150594778+XZeroHakerX@users.noreply.github.com>
2024-11-10 18:28:42 +0100

    commit Terminada

--- app/src/main/java/com/example/app_calendar/MainActivity.kt
+++ app/src/main/java/com/example/app_calendar/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.app_calendar
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.KeyEvent.DispatcherState
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,16 +27,18 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
+    //Declaramos componentes necesarios
     private lateinit var layoutMain: ConstraintLayout
     private lateinit var barMain: ProgressBar
     private lateinit var cardMain: CardView
+    private lateinit var imagenMenu: ImageView
+    private lateinit var botonMenu: Button
 
-
+    //Lista de colores para hacer animaciones y variaciones de color
     val colores = listOf(
         Color.rgb(171,225,136),
         Color.rgb(247,239,153),
-        Color.rgb(130,48,56),
+        Color.rgb(130,58,66),
         Color.rgb(64,121,140)
     )
 
@@ -45,42 +53,119 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         initComponent()
+        initListener()
         rotarBackground()
     }
 
+    private fun initListener() {
+//Inicializamos el boton de seleccionar fecha y creamos una instancia de calendario para manejo de fecha
+// y creamos un DatePickerDialog con la informacion necesaria para luego ejecutar .show()
+        botonMenu.setOnClickListener(){
+            val calendario = Calendar.getInstance()
+            val anio = calendario.get(Calendar.YEAR)
+            val mes = calendario.get(Calendar.MONTH)
+            val dia = calendario.get(Calendar.DAY_OF_MONTH)
 
+            val datePicker = DatePickerDialog(
+                this,
+                {_, anioSelect, mesSelect, diaSelect ->
+                    val estacion = estacion(diaSelect, mesSelect)
+                    pantallaResultado(estacion, anioSelect, mesSelect, diaSelect)
+                },
+                anio,mes,dia
+            )
+            datePicker.show()
+        }
 
+    }
+//Funcion para la llamada de la siguiente actividad Resultado.
+    private fun pantallaResultado(estacion: String, anio: Int, mes: Int, dia: Int ) {
+        val intent = Intent(this,Resultado::class.java)
+        intent.putExtra("estacion", estacion)
+        intent.putExtra("anio", anio)
+        intent.putExtra("mes", mes)
+        intent.putExtra("dia", dia)
+        startActivity(intent)
+
+    }
+//Funcion para comprobar que estacion es la fecha proporcionada.
+    private fun estacion(dia: Int, mes: Int): String {
+
+        if(mes == 0 || mes == 1 || mes == 11 && dia >= 21 || mes == 2 && dia <= 20)
+            return "Invierno"
+
+        if(mes == 3 || mes == 4 || mes == 2 || mes == 5 && dia <= 20)
+            return "Primavera"
+
+        if (mes == 6 || mes == 7 || mes == 5 || mes == 8 && dia <= 20)
+            return "Verano"
+
+        if (mes == 9 || mes == 10 || mes == 8 || mes == 11 )
+            return "Otono"
+
+        return "Desconocido"
+    }
+
+//Funcion con COroutine para hacer las transacciones de color de fondo y demas componentes.
+    @SuppressLint("ResourceAsColor")
     private fun rotarBackground() {
+
         CoroutineScope(Dispatchers.Main).launch {
             var colorIndex = 0
             var colorBar = 2
             var contBar = 25
+//Mientras la actividad esta activa, este bucle no terminará
             while (isActive) {
                 layoutMain.setBackgroundColor(colores[colorIndex])
+
                 barMain.progress = contBar
                 barMain.progressTintList = android.content.res.ColorStateList.valueOf(colores[colorBar])
+
                 cardMain.setCardBackgroundColor(android.content.res.ColorStateList.valueOf(colores[colorBar]))
-                cardMain.alpha = 0.4F
+
+
+                botonMenu.setBackgroundColor(colores[colorBar])
+
                 colorIndex = (colorIndex + 1) % colores.size
                 colorBar = (colorBar + 1) % colores.size
+
+// Cambio de la progresion en la barra de progresos
+                when (contBar){
+
+                   25 -> {
+                          imagenMenu.setImageResource(R.drawable.primavera)
+                          botonMenu.setTextColor(getResources().getColor(R. color. white))
+                          }
+                   50 -> imagenMenu.setImageResource(R.drawable.verano)
+                   75 -> {
+                           imagenMenu.setImageResource(R.drawable.otono)
+                           botonMenu.setTextColor(getResources().getColor(R. color. black))
+                           }
+                   100 -> imagenMenu.setImageResource(R.drawable.invierno)
+
+                }
+//Controlamos el contador de la barra
                 contBar += 25
+
                 if (contBar > 100) {
                     contBar = 25
                 }
-//                when (contBar){
-//                   25 ->
-//                }
-
-
+//Con Delay damos el tiempo de espera entre repeticion den milisegundos
                 delay(2000)
             }
         }
     }
 
+    //Inicializa Componentes que tenemos en .xml
     private fun initComponent() {
         layoutMain = findViewById<ConstraintLayout>(R.id.main)
         barMain = findViewById<ProgressBar>(R.id.progressBar)
-        cardMain = findViewById<CardView>(R.id.cardView)
+        cardMain = findViewById<CardView>(R.id.cardViewImagenes)
+        cardMain.alpha = 0.4F
+        imagenMenu = findViewById<ImageView>(R.id.imagenMenu)
+        botonMenu = findViewById<Button>(R.id.btnMenu)
+
+
     }
 }
 
--- app/src/main/java/com/example/app_calendar/Resultado.kt
+++ app/src/main/java/com/example/app_calendar/Resultado.kt
@@ -1,12 +1,32 @@
 package com.example.app_calendar
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Resultado : AppCompatActivity() {
+    //Declaramos componentes necesarios
+    private lateinit var layoutMain: ConstraintLayout
+    private lateinit var botonAtras: Button
+    private lateinit var textoResult: TextView
+    private lateinit var imagenResult: ImageView
+    private lateinit var textoPoesiaResult: TextView
+
+    //Lista de colores para hacer animaciones y variaciones de color
+    val colores = listOf(
+        Color.rgb(171,225,136),
+        Color.rgb(247,239,153),
+        Color.rgb(130,58,66),
+        Color.rgb(64,121,140)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,17 +37,89 @@ class Resultado : AppCompatActivity() {
             insets
         }
 
+        //Recuperamos los datos necesarios de la actividad anterior
+        val intent = intent
+        var estacion = intent.getStringExtra("estacion")
         initComponent()
         initListeners()
-
+        comprobarFecha(estacion)
 
     }
 
+    //Funcion para comprobar la fecha que viene de la actividad principal
+    private fun comprobarFecha(estacion: String?) {
+
+        if(estacion != null){
+
+            //Comprobacion de estacion para cambiar los colores y textos:
+            if(estacion.equals("Primavera")){
+                textoPoesiaResult.setText(R.string.poesia_primavera)
+                textoResult.setText(R.string.textoPrimavera)
+                textoResult.setTextColor(getColor(R.color.white))
+                botonAtras.setTextColor(getColor(R.color.white))
+                textoPoesiaResult.setTextColor(getColor(R.color.white))
+                botonAtras.setBackgroundColor(colores[2])
+                textoPoesiaResult.setBackgroundColor(colores[2])
+                imagenResult.setImageResource(R.drawable.primavera)
+                layoutMain.setBackgroundColor(colores[0])
+
+
+            }
+            if(estacion.equals("Verano")){
+                textoPoesiaResult.setText(R.string.poesia_verano)
+                textoResult.setText(R.string.textoVerano)
+                textoResult.setTextColor(getColor(R.color.white))
+                botonAtras.setTextColor(getColor(R.color.white))
+                botonAtras.setBackgroundColor(colores[3])
+                textoPoesiaResult.setBackgroundColor(colores[3])
+                textoPoesiaResult.setTextColor(getColor(R.color.white))
+                imagenResult.setImageResource(R.drawable.verano)
+                layoutMain.setBackgroundColor(colores[1])
+            }
+            if(estacion.equals("Otono")){
+                textoPoesiaResult.setText(R.string.poesia_otono)
+                textoResult.setText(R.string.textoOtono)
+                textoResult.setTextColor(getColor(R.color.black))
+                botonAtras.setTextColor(getColor(R.color.black))
+                botonAtras.setBackgroundColor(colores[0])
+                textoPoesiaResult.setBackgroundColor(colores[0])
+                textoPoesiaResult.setTextColor(getColor(R.color.black))
+                imagenResult.setImageResource(R.drawable.otono)
+                layoutMain.setBackgroundColor(colores[2])
+            }
+            if(estacion.equals("Invierno")){
+                textoPoesiaResult.setText(R.string.poesia_invierno)
+                textoResult.setText(R.string.textoInvierno)
+                textoResult.setTextColor(getColor(R.color.black))
+                botonAtras.setTextColor(getColor(R.color.black))
+                botonAtras.setBackgroundColor(colores[1])
+                textoPoesiaResult.setBackgroundColor(colores[1])
+                textoPoesiaResult.setTextColor(getColor(R.color.black))
+                imagenResult.setImageResource(R.drawable.invierno)
+                layoutMain.setBackgroundColor(colores[3])
+            }
+        }else{
+            textoPoesiaResult.setText("Error")
+            textoResult.setText("Error")
+        }
+    }
+
+    //Añade los listeners a los componentes
     private fun initListeners() {
-        TODO("Not yet implemented")
+
+        botonAtras.setOnClickListener{
+            finish()
+        }
     }
 
+    //Inicializa Componentes que tenemos en .xml
     private fun initComponent() {
-        TODO("Not yet implemented")
+        layoutMain = findViewById(R.id.main)
+        botonAtras = findViewById(R.id.botonAtras)
+        textoResult = findViewById(R.id.textoResultado)
+        imagenResult = findViewById(R.id.imagenResultado)
+        textoPoesiaResult = findViewById(R.id.textoPoesiaResult)
+
+
     }
 }
